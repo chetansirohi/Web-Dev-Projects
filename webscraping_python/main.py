4d65a62e119793e1178a4d3948ea22fa91e7dfb2
chetansirohi <chetansirohi36@gmail.com>
2021-01-27 22:39:02 +0530

    'adding web scraping'

--- webscraping_python/main.py
+++ webscraping_python/main.py
@@ -4,6 +4,9 @@ with open('home.html','r') as html_file:
     content=html_file.read()
 
     soup=BeautifulSoup(content,'lxml')
-    tags=soup.find_all('h5'
-                       )
-    print(tags)
+    course_cards=soup.find_all('div',class_='card')
+    for course in course_cards:
+        course_name=course.h5.text
+        course_price=course.a.text.split()[-1]
+
+        print(f'{course_name} costs {course_price}')

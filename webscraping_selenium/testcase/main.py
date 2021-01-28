a1c131b5a6a7c9d223b6dfc422d1e1278353a115
chetansirohi <chetansirohi36@gmail.com>
2021-01-28 22:07:18 +0530

    page setup

--- webscraping_selenium/testcase/main.py
+++ webscraping_selenium/testcase/main.py
@@ -9,6 +9,10 @@ class PythonOrgSeach(unittest.TestCase):
 
     # def test_example(self):#anything with keyword test runs upon execution
     
+    def test_title(self):
+        mainPage=page.MainPage()
+        assert mainPage.is_title_matches()
+
 
     def tearDown(self):#cleanup
         self.driver.close()
